8b0bf01a9d7223d3c6f4725a2d5c68ef777d56a3
Robbie Straw <drbawb@fatalsyntax.com>
2016-03-31 12:21:33 -0500

    handle basic commands

--- src/main.rs
+++ src/main.rs
@@ -9,103 +9,158 @@ struct Repl {
     stack: Vec<f64>, // todo: deque?
 }
 
-macro_rules! impl_bin_op {
-    ($x:ident, $y:ident, $result:expr) => {{
-         match ($x,$y) {
-            (Some($x), Some($y)) => { Ok($result) },
-            _ => { Err("need two operands on stack") },
-        }
-    }}
-}
-
 impl Repl {
     pub fn new() -> Repl {
         Repl { stack: vec![] }
     }
 
+    pub fn consume(&mut self, buf: &str) -> Result<(), Box<Error>> {
+        Ok(self.stack.push(try!(buf.parse())))
+    }
+
+    pub fn draw(&self, term: &RustBox) {
+        let ofs = 3;
+        let mut stack = self.stack.iter().rev();
+        for i in 0..10 {
+            let idx = 9 - i;
+            let data = match stack.next() {
+                Some(reg) => format!("{}", reg),
+                None => "".to_string(),
+            };
+
+            let output = format!("{:02}: {}", i+1, data);
+            tclear(term, (ofs + idx), 1, term.width());
+            twrite(term, (ofs + idx), 1, &output[..]);
+        }
+    }
+
     // basic ops
+    fn grab_two(&mut self) -> (f64, f64) {
+        assert!(self.stack.len() >= 2);
+        let y = self.stack.pop().unwrap();
+        let x = self.stack.pop().unwrap();
+        (x, y)
+    }
+
     pub fn add(&mut self) -> Result<(), &'static str> {
-        let y = self.stack.pop();
-        let x = self.stack.pop();
-        impl_bin_op!(x, y, self.stack.push(x + y))
+        if self.stack.len() < 2 { return Err("need two operands"); }
+        let (x, y) = self.grab_two();
+        Ok(self.stack.push(x + y))
     }
 
     pub fn sub(&mut self) -> Result<(), &'static str> {
-        let y = self.stack.pop();
-        let x = self.stack.pop();
-        impl_bin_op!(x, y, self.stack.push(x - y))
+        if self.stack.len() < 2 { return Err("need two operands"); }
+        let (x, y) = self.grab_two();
+        Ok(self.stack.push(x - y))
     }
 
     pub fn mul(&mut self) -> Result<(), &'static str> {
-        let y = self.stack.pop();
-        let x = self.stack.pop();
-        impl_bin_op!(x, y, self.stack.push(x * y))
+        if self.stack.len() < 2 { return Err("need two operands"); }
+        let (x, y) = self.grab_two();
+        Ok(self.stack.push(x * y))
     }
 
     pub fn div(&mut self) -> Result<(), &'static str> {
-        let y = self.stack.pop();
-        let x = self.stack.pop();
-        impl_bin_op!(x, y, self.stack.push(x / y))
+        if self.stack.len() < 2 { return Err("need two operands"); }
+        let (x, y) = self.grab_two();
+        Ok(self.stack.push(x / y))
     }
 }
 
 
 
 fn main() {
-    let term = RustBox::init(Default::default())
-        .ok().expect("could not open term...");
-
-
-    twrite(&term, 1, 1, "hello, world");
-    twrite(&term, 3, 1, "press 'q' to quit");
-    twrite(&term, 4, 1, "press 'l' to lol");
-    term.present();
-
+    let mut repl = Repl::new();
     let mut input = format!("");
 
+    let term = RustBox::init(Default::default())
+                   .ok()
+                   .expect("could not open term...");
+
+    twrite(&term, 0, 1, "ripple - input mode");
+    twrite(&term, 1, 1, "q: quit, ?: help   ");
+    term.present();
+
     loop {
+        repl.draw(&term);
+        tinput(&term, 15, 1, &input[..]);
+        term.present();
+
+        // clear buffers
+        tclear(&term, 14, 1, term.width());
+
         match term.poll_event(false) {
             Ok(Event::KeyEvent(keycap)) => {
                 match keycap {
-                    Key::Char('q') => { break; },
-                    Key::Char('l') => {
-                        twrite(&term, 5, 1, "what the shit?");
+                    Key::Char('q') => break,
+
+                    Key::Char('+') => match repl.add() {
+                        Err(msg) => terror(&term, 14, 1, msg),
+                        _ => {}
                     },
 
-                    Key::Char(num) if is_numeric(num) => {
-                        input.push(num);
-                        tinput(&term, 10, 1, &input[..]);
+                    Key::Char('-') =>match repl.sub() {
+                        Err(msg) => terror(&term, 14, 1, msg),
+                        _ => {}
                     },
+
+                    Key::Char('*') => match repl.mul() {
+                        Err(msg) => terror(&term, 14, 1, msg),
+                        _ => {}
+                    },
+
+                    Key::Char('/') => match repl.div() {
+                        Err(msg) => terror(&term, 14, 1, msg),
+                        _ => {}
+                    },
+
+                    Key::Char(num) if is_numeric(num) => input.push(num), 
 
                     Key::Enter => {
-                        tclear(&term, 10, 1, input.len());
+                        match repl.consume(&input[..]) {
+                            Err(e) => terror(&term, 14, 1, e.description()),
+                            _ => {}
+                        };
+
+                        tclear(&term, 15, 1, input.len());
                         input.clear();
-                    },
+                    }
 
-                    _ => {},
+                    Key::Backspace => {
+                        tclear(&term, 15, 1, input.len());
+                        let new_length = input.len() - 1;
+                        if new_length >= 0 { input.truncate(new_length); }
+                    }
+
+                    _ => terror(&term, 14, 1, "unhandled keypress"),
                 }
-            },
+            }
 
-            Ok(_)  => { 
-                println!("unknown key"); 
-            },
+            Ok(_) => {
+                terror(&term, 14, 1, "unhandled event");
+            }
 
-            Err(e) => { panic!("err: {}", e.description()); },
+            Err(e) => {
+                panic!("err: {}", e.description());
+            }
         }
-
-        term.present();
     }
 }
 
 fn is_numeric(input: char) -> bool {
     match input {
-         '0'...'9' => { true },
-         _ => { false },
+        '0'...'9' => true,
+        '.' => true, // TODO: not technically true, can only have one ?
+        _ => false,
     }
 }
 
 fn twrite(term: &RustBox, row: usize, col: usize, text: &str) {
     term.print(col, row, rustbox::RB_BOLD, Color::White, Color::Black, text);
+}
+
+fn terror(term: &RustBox, row: usize, col: usize, text: &str) {
+    term.print(col, row, rustbox::RB_BOLD, Color::Red, Color::Black, text);
 }
 
 fn tinput(term: &RustBox, row: usize, col: usize, text: &str) {
@@ -114,6 +169,11 @@ fn tinput(term: &RustBox, row: usize, col: usize, text: &str) {
 
 fn tclear(term: &RustBox, row: usize, col: usize, len: usize) {
     for i in 0..len {
-        term.print(col+i, row, rustbox::RB_BOLD, Color::Default, Color::Default, " ");
+        term.print(col + i,
+                   row,
+                   rustbox::RB_BOLD,
+                   Color::Default,
+                   Color::Default,
+                   " ");
     }
 }
