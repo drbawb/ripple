a17a796544574433244c3f5582e6c8a1a4ffd401
Robbie Straw <drbawb@fatalsyntax.com>
2016-04-01 15:06:25 -0500

    add dummy impl of win32 platform

---
+++ src/platform/win.rs
@@ -0,0 +1,17 @@
+use super::{Color, Terminal};
+use super::Event;
+
+struct WinConsole;
+
+impl Terminal for WinConsole {
+    fn width(&self)  -> usize { 0 }
+    fn height(&self) -> usize { 0 }
+
+    fn move_cursor(&mut self, row: usize, col: usize) {}
+    fn color_cursor(&mut self, bg: Color, fg: Color) {}
+    fn render(&self) {}
+
+    fn poll_event(&self) -> Result<Event, String> { unimplemented!() }
+    fn write_ln(&self, text: &str) {}
+    fn clear_ln(&self) {}
+}
